1e1a675dc8825bb0ac740622bfcde7f4a5c52804
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-19 10:40:53 -0500

    Installed Vapor, Authentication, and JWT packages

--- Package.swift
+++ Package.swift
@@ -8,10 +8,12 @@ let package = Package(
         .library(name: "JWTMiddlware", targets: ["JWTMiddlware"]),
     ],
     dependencies: [
-        
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc")
     ],
     targets: [
-        .target(name: "JWTMiddlware", dependencies: []),
+        .target(name: "JWTMiddlware", dependencies: ["Vapor", "Authentication", "JWT"]),
         .testTarget(name: "JWTMiddlwareTests", dependencies: ["JWTMiddlware"]),
     ]
 )
