32ba8e4e47ff95da80f8af8f17effeab3ff1a910
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-19 11:57:19 -0500

    Installed VaporRequestStorage package

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
         .library(name: "JWTMiddlware", targets: ["JWTMiddlware"]),
     ],
     dependencies: [
+    .package(url: "https://github.com/skelpo/vapor-request-storage.git", from: "0.1.0"),
         .package(url: "https://github.com/skelpo/JWTVapor.git", from: "0.6.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
     ],
     targets: [
-        .target(name: "JWTAuthenticatable", dependencies: ["Vapor", "Authentication", "JWTVapor"]),
+        .target(name: "JWTAuthenticatable", dependencies: ["Vapor", "Authentication", "JWTVapor", "VaporRequestStorage"]),
         .target(name: "JWTMiddlware", dependencies: ["Vapor", "Authentication", "JWTVapor"]),
         .testTarget(name: "JWTMiddlwareTests", dependencies: ["JWTMiddlware"])
     ]
