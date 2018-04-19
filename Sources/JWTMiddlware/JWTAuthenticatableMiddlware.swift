a533ccfdaec6394ff8727a2fc2b388734f035111
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-19 13:56:02 -0500

    Documented JWTAuthenticatableMiddlware class

--- Sources/JWTMiddlware/JWTAuthenticatableMiddlware.swift
+++ Sources/JWTMiddlware/JWTAuthenticatableMiddlware.swift
@@ -5,27 +5,47 @@ import Crypto
 import Vapor
 import JWT
 
-public final class JWTAuthenticatableMiddlware<A: JWTAuthenticatable>: Middleware where A.Database: QuerySupporting {
+/// Handles authenticating/authorizing a model conforming to `JWTAuthenticatable`
+/// using data pulled from a request.
+public final class JWTAuthenticatableMiddlware<A: JWTAuthenticatable>: Middleware {
+    
+    // We create this empty init because the
+    // synthesized init is marked `internal`.
+    
+    /// Creates an instance of the middleware
     public init() {}
     
+    /// See Middleware.respond(to:chainingTo:).
     public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
+        
+        // If the model is already quthenticated,
+        // no more work for us! Fire the next responder.
         if try request.isAuthenticated(A.self) {
             return try next.respond(to: request)
             
+        // Check to see if an `AuthBody` instance can be created.
         } else if let payload = try A.authBody(from: request) {
+            
+            // We got an `AuthBody` instance. Authenticate the model, then fire the next responder.
             return try A.authenticate(from: payload, on: request).flatMap(to: Response.self) { authenticated in
                 return try next.respond(to: request)
             }
-            
+        
+        // Check to see if a `Authorization: Bearer ...` header exists.
         } else if request.http.headers.bearerAuthorization != nil {
+            
+            // Header found. Get the payload from the request.
             let payload: A.Payload = try request.payload()
             
+            // Authenticate the model, then fire the next responder.
             return try A.authenticate(from: payload, on: request).flatMap(to: Response.self) { model in
                 return try next.respond(to: request)
             }
             
         } else {
-            throw Abort(.unauthorized, reason: "No authorized user to data to authorize a user was found")
+            
+            // No Authorized model or data to auth found. Throw a 401 (Unauthorized) error.
+            throw Abort(.unauthorized, reason: "No authorized user or data to authorize a user was found")
         }
     }
 }
