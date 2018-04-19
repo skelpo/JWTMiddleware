a7133894df0d8ce842d5b29f91d5d8d5066ddce1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-19 10:49:58 -0500

    Created IdentifiableJWTPayload and JWTAuthenticatable protocols

---
+++ Sources/JWTMiddlware/JWTAuthenticatable.swift
@@ -0,0 +1,21 @@
+import Authentication
+import Vapor
+import JWT
+
+public protocol IdentifiableJWTPayload: JWTPayload {
+    associatedtype ID
+    
+    var id: ID { get }
+}
+
+public protocol JWTAuthenticatable: Authenticatable, Content where Payload.ID == Self.ID {
+    associatedtype AuthBody
+    associatedtype Payload: IdentifiableJWTPayload
+    
+    func accessToken(on request: Request) throws -> EventLoopFuture<Payload>
+    
+    static func authBody(from request: Request)throws -> AuthBody?
+    static func authenticate(from payload: Payload, on request: Request)throws -> Future<Self>
+    static func authenticate(from body: AuthBody, on reques: Request)throws -> Future<Self>
+}
+
