178a91fef57bf807336bdd5754e300f82d73e8a2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-02 15:03:00 -0500

    Renamed JWTVerificationMiddleware<Payload> to JWTStorageMiddleware<Payload>. Created JWTVerificationMiddleware

--- Sources/JWTMiddleware/JWTVerificationMiddleware.swift
+++ Sources/JWTMiddleware/JWTVerificationMiddleware.swift
@@ -4,7 +4,7 @@ import Vapor
 import JWT
 
 /// Gets a JWT payload from a request, validates it, and stores it for later.
-public final class JWTVerificationMiddleware<Payload: JWTPayload>: Middleware {
+public final class JWTStorageMiddleware<Payload: JWTPayload>: Middleware {
     
     // We create this empty init because the
     // synthesized init is marked `internal`.
@@ -24,5 +24,37 @@ public final class JWTVerificationMiddleware<Payload: JWTPayload>: Middleware {
         
         try request.set("skelpo-payload", to: payload)
         return try next.respond(to: request)
+    }
+}
+
+
+/// Validates a JWT token in a request's `Auithorization` header.
+public final class JWTVerificationMiddleware: Middleware {
+    
+    // We create this empty init because the
+    // synthesized init is marked `internal`.
+    
+    /// Creates an instance of the middleware
+    public init() {}
+    
+    /// See Middleware.respond(to:chainingTo:).
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
+        
+        // Extract the token from the request. It is expected to
+        // be in the `Authorization` header as a bearer: `Bearer ...`
+        guard let token = request.http.headers.bearerAuthorization?.token else {
+            throw Abort(.badRequest, reason: "'Authorization' header with bearer token is missing")
+        }
+        
+        // Get JWT service to verify the token with
+        let jwt = try request.make(JWTService.self)
+        let data: Data = Data(token.utf8)
+        
+        // Verify to tokens data.
+        if try jwt.verify(data) {
+            return try next.respond(to: request)
+        } else {
+            throw Abort(.forbidden, reason: "JWT verification failed")
+        }
     }
 }
